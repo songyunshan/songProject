56d9e09ba3f243845ae6525b014a804be3687d16
songyunshan <songyunshan90@163.com>
2019-03-19 14:30:45 +0800

    提交算法

---
+++ venv/python/seleniumTest.py
@@ -0,0 +1,5 @@
+#encoding=utf-8
+from selenium import webdriver
+#打开浏览器
+driver = webdriver.Chrome("C:\\Users\\sys\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver")
+driver.get("https://www.baidu.com")
